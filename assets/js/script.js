7ac11481f129aee4ef1d5f97407984dfb1aec362
JamesMackman <>
2023-11-01 11:04:35 +0000

    Set up strucutre of quiz functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Function to load questions from the database
+function loadQuestions() {
+    
+}
+
+// Function to display questions and answer options
+function displayQuestion() {
+    
+}
+
+// Function to handle user's answer selection
+function handleAnswerSelection() {
+    
+}
+
+// Function to update the score and progress
+function updateScoreAndProgress() {
+    
+}
+
+// Function to display the final result
+function displayResult() {
+    
+}
+
+// Function to handle the click event on the next button
+function handleNextButtonClick() {
+
+}
+
+// Function to initialize the quiz
+function initializeQuiz() {
+
+}
+
+// Call the initializeQuiz function when the window loads
+window.onload = function () {
+    initializeQuiz();
+};
