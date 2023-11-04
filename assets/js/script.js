296abde90f6692545187f961333457ecd35aca6f
JamesMackman <>
2023-11-04 21:53:06 +0000

    Update retry button to change color once quiz has been complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,86 +51,77 @@ const questions = [
     }
 ];
 
-// Initialize variables
 let score = 0;
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let selectedAnswer = null;
 let progress = 0;
 let progressBarValue = 0;
+let originalButtonColor; // Variable to store the original color of the retry button
 
-// Function to load questions sequentially
 function loadQuestions() {
     if (progress >= questions.length) {
-        // Handle the end of the quiz
         const progressBar = document.getElementById("progressBar");
         progressBarValue = progressBar.max; // Set the progress bar to full
         progressBar.value = progressBarValue;
         alert(
             `Quiz complete! Your final score is ${score}. You answered ${correctAnswers} out of ${questions.length} questions correctly.`
         );
-        // Reset the variables for a new quiz
         progress = 0;
         score = 0;
         correctAnswers = 0;
         incorrectAnswers = 0;
-        progressBarValue = 0; // Reset the progress bar value
-        updateScoreAndProgress(); // Update the UI with reset values
+        progressBarValue = 0;
+        updateScoreAndProgress();
+        updateRetryButtonColor(true); // Update the retry button color here with a flag to indicate the color change
     } else {
         const currentQuestion = questions[progress];
         displayQuestion(currentQuestion);
     }
 }
 
-// Function to display questions and answer options
 function displayQuestion(question) {
-    // Display the question
     const questionContainer = document.getElementById("questionText");
     questionContainer.textContent = question.question;
 
-    // Display the answer options
     const answerContainer = document.getElementById("answerOptions");
-    answerContainer.innerHTML = ""; // Clear previous options
+    answerContainer.innerHTML = "";
 
     question.options.forEach((option, index) => {
         const optionElement = document.createElement("td");
         optionElement.classList.add("answer-option");
         optionElement.textContent = option;
-        optionElement.setAttribute("data-option", option); // Set a data attribute for the option
+        optionElement.setAttribute("data-option", option);
         answerContainer.appendChild(optionElement);
 
         optionElement.addEventListener('click', () => {
             if (selectedAnswer === option) {
-                // Deselect the option if it is already selected
                 selectedAnswer = null;
-                optionElement.style.backgroundColor = ""; // Reset the background color
+                optionElement.style.backgroundColor = "";
             } else {
-                // Change the selected answer when a different option is clicked
                 const prevOption = answerContainer.querySelector(
                     `[data-option="${selectedAnswer}"]`
                 );
                 if (prevOption) {
-                    prevOption.style.backgroundColor = ""; // Reset the background color of the previous selection
+                    prevOption.style.backgroundColor = "";
                 }
-                selectedAnswer = option; // Update the selected answer
-                optionElement.style.backgroundColor = "lightblue"; // Add visual indication for the selected option
+                selectedAnswer = option;
+                optionElement.style.backgroundColor = "lightblue";
             }
         });
     });
 }
 
-// Function to update the progress bar
 function updateProgressBar() {
     const progressBar = document.getElementById("progressBar");
     if (progressBarValue < progressBar.max) {
         progressBarValue += 1;
         progressBar.value = progressBarValue;
     } else {
-        loadQuestions(); // Reset the quiz when the progress bar is filled
+        loadQuestions();
     }
 }
 
-// Function to handle the click event on the next button
 function handleNextButtonClick() {
     if (progress >= questions.length) {
         alert("The quiz is already complete. Please press the 'Retry' button to start over.");
@@ -143,56 +134,71 @@ function handleNextButtonClick() {
         const currentQuestion = questions[progress];
         if (selectedAnswer === currentQuestion.answer) {
             alert("Correct!");
-            score++; // Increment the score for a correct answer
-            correctAnswers++; // Increment the count of correct answers
+            score++;
+            correctAnswers++;
         } else {
             alert(`Incorrect. The correct answer is: ${currentQuestion.answer}`);
-            incorrectAnswers++; // Increment the count of incorrect answers
+            incorrectAnswers++;
         }
-        progress++; // Increase the progress
-        updateScoreAndProgress(); // Update the score and progress immediately
+        progress++;
+        updateScoreAndProgress();
 
         if (progress >= questions.length) {
             const nextButton = document.getElementById("nextButton");
-            nextButton.disabled = true; // Disable the next button
+            nextButton.disabled = true;
         }
 
-        updateProgressBar(); // Update the progress bar if the quiz is not yet complete
-        loadQuestions(); // Load the next question
+        updateProgressBar();
+        loadQuestions();
     }
 }
 
-// Function to handle the click event on the retry button
 function handleRetryButtonClick() {
-    // Reset the variables and the progress bar
     score = 0;
     correctAnswers = 0;
     incorrectAnswers = 0;
     progress = 0;
-    progressBarValue = 0; // Reset the progress bar value
+    progressBarValue = 0;
     const progressBar = document.getElementById("progressBar");
-    progressBar.value = 0; // Reset the progress bar value in the HTML
-    updateScoreAndProgress(); // Update the UI with reset values
-    loadQuestions(); // Reset the quiz
+    progressBar.value = 0;
+    updateScoreAndProgress();
+
+    const nextButton = document.getElementById("nextButton");
+    nextButton.disabled = false;
+
+    const retryButton = document.getElementById("retryButton");
+    retryButton.style.backgroundColor = "red";
+    loadQuestions();
+
+    resetRetryButtonColor(); // Reset the retry button color
 }
 
-// Call the handleRetryButtonClick function when the retry button is clicked
 const retryButton = document.getElementById("retryButton");
 retryButton.addEventListener("click", handleRetryButtonClick);
 
+function updateRetryButtonColor(changeColor) {
+    const retryButton = document.getElementById("retryButton");
+    if (changeColor) {
+        originalButtonColor = retryButton.style.backgroundColor; // Store the original button color
+        retryButton.style.backgroundColor = "red"; // Change the background color of the retry button to red
+    }
+}
 
-// Function to update the score based on the user's answer
+function resetRetryButtonColor() {
+    const retryButton = document.getElementById("retryButton");
+    retryButton.style.backgroundColor = originalButtonColor; // Reset the background color of the retry button
+}
+
 function updateScore(isCorrect) {
     if (isCorrect) {
         score++;
         correctAnswers++;
     } else {
-        incorrectAnswers = Math.max(0, incorrectAnswers + 1); // Increment the count of incorrect answers by 1, starting from 0
+        incorrectAnswers = Math.max(0, incorrectAnswers + 1);
     }
-    updateScoreAndProgress(); // Call the function to update the UI with the new values
+    updateScoreAndProgress();
 }
 
-// Function to update the score and progress
 function updateScoreAndProgress() {
     const scoreElement = document.getElementById("scoreValue");
     const correctElement = document.getElementById("correctAnswers");
@@ -202,11 +208,8 @@ function updateScoreAndProgress() {
     incorrectElement.textContent = incorrectAnswers;
 }
 
-// Function to initialize the quiz
 function initializeQuiz() {
     loadQuestions();
 }
 
-// Call the initializeQuiz function when the window loads
 window.onload = initializeQuiz;
-
